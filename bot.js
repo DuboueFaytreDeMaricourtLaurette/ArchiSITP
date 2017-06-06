ec7be3199fb6a3bfd548a525b0f1e310145ae4a9
AlizeeFaytre <afaytre@juniorisep.com>
2017-06-06 20:49:10 +0200

    open weather map fonctionne ... mais donne pas encore la bonne ville :/

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
 
+var clientOWM = require('node-rest-client-promise').Client()
+
 client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
@@ -14,6 +16,34 @@ client.on('message', msg => {
   // If message is hello, post hello too
   if (msg.content === 'hello') {
     msg.channel.sendMessage('Hello to you too, fellow !')
+  }
+
+  var msgTab = msg.content.split(' ')
+
+  if (msgTab[0] === 'weather:') {
+    var messageOWM = ''
+    for (var j = 1; j < msgTab.length; j++) {
+      messageOWM = messageOWM + ' ' + msgTab[j]
+    }
+
+    var newMessageOWM = messageOWM.split(', ')
+
+    var city = newMessageOWM[0]
+    var country = newMessageOWM[1]
+    console.log(messageOWM)
+    console.log(city)
+    console.log(country)
+    clientOWM.getPromise('http://api.openweathermap.org/data/2.5/weather?q=city,country&units=metric&lang=fr&APPID=b05787eda8d8f7967925692ea52134d2')
+      .catch((error) => {
+        throw error
+      })
+      .then((res) => {
+        var weather = 'La température est de ' + res.data.main.temp + '°C'
+        weather = weather + ', l\'humidité est de ' + res.data.main.humidity + ' %'
+        weather = weather + ', le temps est : ' + res.data.weather[0].description
+        msg.channel.sendMessage(weather)
+        console.log(res.data)
+      })
   }
 })
 

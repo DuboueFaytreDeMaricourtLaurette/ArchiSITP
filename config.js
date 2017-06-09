2e95d39991b07c345d766d407c757ea5c75c63df
AudeDM <aude.demaricourt@gmail.com>
2017-06-09 10:18:20 +0200

    spotify marche!

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
+
   token: 'MzEyNTAyMDUxNDkyMDY5Mzg2.DAmEtg.AUxQC5L8MMu9qP17Fu3pdh6q0yY',
   channel: '307411336084586496'
+
 }
 /* module.exports = {
   token: 'MzEyNTAyMDUxNDkyMDY5Mzg2.DB7PpQ.FSS7CATo0FmIPBc0stPzEm5HW48',

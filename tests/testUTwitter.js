dc9da130d4202822604848fef821a30c70609d2f
AlizeeFaytre <afaytre@juniorisep.com>
2017-06-11 18:05:39 +0200

    test unitaire pour tweeter fait

--- tests/testUTwitter.js
+++ tests/testUTwitter.js
@@ -1,14 +1,20 @@
 import test from 'ava'
-var client = require('node-rest-client-promise').Client()
+
+var Twitter = require('twitter')
+
+var twitterClient = new Twitter({
+  consumer_key: 'GbhsMXiYxEurst8y9iRdTLnXY',
+  consumer_secret: 'kjGHlQNEv7BVJZkR27y5y2T4nviQfzq3m4zzKKosmZsw1w4xxk',
+  access_token_key: '865505348249047040-Ndur0ufaJrKX9sDWG2AqK6N6Cld54YT',
+  access_token_secret: 'OYTFpWH9jXkWndG08XgYkgvmOoCx0f6SHaJ5e8LAMTSq3'
+})
 
 test('Example test', t => {
-  return client.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
-    .catch((error) => {
+  return twitterClient.post('statuses/update', {status: 'tweet du test unitaire'}, function (error, tweet, response) {
+    if (!error) {
+      t.is(response.statusCode, 200)
+    } else {
       t.fail()
-      throw error
-    })
-    .then((res) => {
-      console.log(res.response.statusCode)
-      t.is(res.response.statusCode, 200)
-    })
+    }
+  })
 })

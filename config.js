54dec55956db5a5ba3c82299a34989f903018ecf
estherlaurette <esther.laurette@gmail.com>
2017-06-12 14:27:04 +0200

    fixing conflicts

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   token: 'MzEyNTAxNTU2Njg1ODMyMTky.C_cCLQ.YK8__EWTI7SwT-I5BovMqWHi898',
   channel: 'channelid'
 }
--- tests/example.js
+++ tests/example.js
@@ -2,7 +2,7 @@ import test from 'ava'
 var client = require('node-rest-client-promise').Client()
 
 test('Example test', t => {
-  return client.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
+  return client.getPromise('https://www.googleapis.com/youtube/v3/search?q=rick&part=snippet&key=AIzaSyDLAMBpwv-YF3aZEif6RBtdMem8YVtFZSY')
     .catch((error) => {
       t.fail()
       throw error
